c104debe49ed7c32f6d4e89b86168d63790e1a5a
craole <iamcraole@gmail.com>
2023-02-23 15:15:10 -0500

    convert flake to template

--- flake.nix
+++ flake.nix
@@ -5,17 +5,28 @@
 #|   │   ├── default.nix
 #|   │   └── devShell.nix
 #|   └── flake.nix
+
 #@ Project File
 #| flake.nix
 #>============================================<#
 
 {
-  description = "My Nix Development Environment";
+  description = "Development Environment";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    official-templates.url = github:NixOS/templates;
   };
-  outputs = { self, nixpkgs, flake-utils }: {
-    defaultPackage = import ./configuration { pkgs = nixpkgs; };
+
+  outputs = { self, official-templates, ... }: {
+    templates = {
+      rust-fenix = {
+        path = ./templates/rust_fenix;
+        description = "Rust Development Environment [Nightly], using fenix";
+      };
+      rust-hello = {
+        path = ./templates/rust_hello;
+        description = "Simple Hello World in Rust";
+      };
+    }
+    // official-templates.templates;
   };
 }
