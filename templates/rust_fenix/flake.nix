cf463ba095d887ff5b2b6d4fe963b6d1086d3a2c
craole <iamcraole@gmail.com>
2023-02-24 17:12:09 -0500

    enabling fenix template

--- templates/rust_fenix/flake.nix
+++ templates/rust_fenix/flake.nix
@@ -5,7 +5,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs-unstable";
   };
 
   outputs = { self, fenix, flake-utils, nixpkgs }:
