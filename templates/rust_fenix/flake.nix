898424e5c164983235b165b8afcc349758907865
Craole <iamcraole@gmail.com>
2023-03-31 06:48:10 -0500

    "fenix not working out"

--- templates/rust_fenix/flake.nix
+++ templates/rust_fenix/flake.nix
@@ -1,41 +1,54 @@
 {
+  description = "A Nix-flake-based Rust development environment";
+
   inputs = {
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
     flake-utils.url = "github:numtide/flake-utils";
-    naersk = {
-      url = "github:nix-community/naersk";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, fenix, flake-utils, naersk, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      packages.default =
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          target = "aarch64-unknown-linux-gnu";
-          toolchain = with fenix.packages.${system}; combine [
-            minimal.cargo
-            minimal.rustc
-            targets.${target}.latest.rust-std
-          ];
-        in
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , rust-overlay
+    }:
 
-        (naersk.lib.${system}.override {
-          cargo = toolchain;
-          rustc = toolchain;
-        }).buildPackage {
-          src = ./.;
-          CARGO_BUILD_TARGET = target;
-          CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER =
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      overlays = [
+        (import rust-overlay)
+        (self: super: {
+          rustToolchain =
             let
-              inherit (pkgs.pkgsCross.aarch64-multiplatform.stdenv) cc;
+              rust = super.rust-bin;
             in
-            "${cc}/bin/${cc.targetPrefix}cc";
-        };
+            if builtins.pathExists ./rust-toolchain.toml then
+              rust.fromRustupToolchainFile ./rust-toolchain.toml
+            else if builtins.pathExists ./rust-toolchain then
+              rust.fromRustupToolchainFile ./rust-toolchain
+            else
+              rust.stable.latest.default;
+        })
+      ];
+
+      pkgs = import nixpkgs { inherit system overlays; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          rustToolchain
+          openssl
+          pkg-config
+          cargo-deny
+          cargo-edit
+          cargo-watch
+          rust-analyzer
+        ];
+
+        shellHook = ''
+          ${pkgs.rustToolchain}/bin/cargo init
+        '';
+      };
     });
 }
