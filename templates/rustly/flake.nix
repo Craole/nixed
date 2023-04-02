03e4b9ff006058b5d3d05ff380d9a5575580c679
Craole <iamcraole@gmail.com>
2023-04-02 11:08:36 -0500

    "trying to add vscode to the rustly flake"

--- templates/rustly/flake.nix
+++ templates/rustly/flake.nix
@@ -1,32 +1,32 @@
 {
   description = "Rust Nightly Development Environment";
-  inputs =
-    {
-      nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-      flake-utils.url = "github:numtide/flake-utils";
-      rust-overlay.url = "github:oxalica/rust-overlay";
-    };
-  outputs =
-    { self, nixpkgs, flake-utils, rust-overlay }:
-
-    flake-utils.lib.eachDefaultSystem (system:
-    let
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
       overlays = [
         (import rust-overlay)
         (self: super: {
-          rustToolchain = super.rust-bin.fromRustupToolchainFile ./Rust.toml;
+          rustToolchain = super.rust-bin.fromRustupToolchainFile ./rust-toolchain;
         })
       ];
-      pkgs = import nixpkgs { inherit system overlays; };
-    in
-    {
+      pkgs = import nixpkgs {inherit system overlays;};
+    in {
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
           #/> Language-specific <\#
           rustToolchain
-          # rust-analyzer
+          rust-analyzer
           # rust-analyzer-nightly
-          rust-analyzer-unwrapped
+          # rust-analyzer-unwrapped
           openssl
           pkg-config
           cargo-edit
@@ -39,55 +39,72 @@
 
           #/> Editor <\#
           (vscode-with-extensions.override {
-            vscodeExtensions = with vscode-extensions; [
-              bbenoist.nix
-              ms-python.python
-              ms-azuretools.vscode-docker
-              ms-vscode-remote.remote-ssh
-            ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-              {
-                name = "remote-ssh-edit";
-                publisher = "ms-vscode-remote";
-                version = "0.47.2";
-                sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-              }
-            ];
+            vscodeExtensions = with vscode-extensions;
+              [
+                #| Rust
+                rust-lang.rust-analyzer
+                serayuzgur.crates
+
+                #| TOML
+                tamasfe.even-better-toml
+
+                #| ShellScript
+                timonwong.shellcheck
+
+                #| Nix
+                kamadorueda.alejandra
+                jnoortheen.nix-ide
+                bbenoist.nix
+                mkhl.direnv
+
+                #| Web Development
+                bradlc.vscode-tailwindcss
+
+                #| Settings
+                vadimcn.vscode-lldb
+                formulahendry.code-runner
+                vscode-icons-team.vscode-icons
+                ms-vscode-remote.remote-ssh
+              ]
+              ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+                {
+                  name = "remote-ssh-edit";
+                  publisher = "ms-vscode-remote";
+                  version = "0.47.2";
+                  sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+                }
+              ];
           })
-          # vscode
-          # vscode-extensions.rust-lang.rust-analyzer-nightly
         ];
 
         shellHook = ''
           #/> Aliases <\#
-          ls() {
-            exa \
+          alias ls='exa \
           	--icons \
           	--color-scale \
           	--header \
           	--no-user \
           	--git \
           	--group-directories-first \
-          	--sort=.name \
-          	"$@"
-          }
-          ll(){ ls --all --long "$@" ;}
-          cR(){ cargo run --quiet ;}
-          cW(){
-            cargo watch \
+            --all \
+            --long \
+          	--sort=.name
+          '
+          alias ll='ls --all --long'
+          alias cR='cargo run --quiet'
+          alias cW='cargo watch \
               --quiet \
               --clear \
               --exec \
               "run --quiet --"
-          }
-          rV(){ rustc -vV ;}
-          codeEdit(){ code . ;}
+          '
+          alias rV='rustc -vV'
+          alias codeEdit='code .'
           alias find="fd"
           alias grep="rg"
 
           #/> Autostart <\#
-          # ${pkgs.rustToolchain}/bin/rustc -vV
-          # ${pkgs.rustToolchain}/bin/cargo run --quiet
-          rV && cR && codeEdit
+          rV && cR
         '';
       };
     });
