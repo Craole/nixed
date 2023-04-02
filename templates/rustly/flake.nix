68a4526f8b8a4a50e2e834aa2c99f81e912aff27
Craole <iamcraole@gmail.com>
2023-04-02 06:49:08 -0500

    "trying to add vscode to the rustly flake"

--- templates/rustly/flake.nix
+++ templates/rustly/flake.nix
@@ -38,8 +38,23 @@
           ripgrep
 
           #/> Editor <\#
-          vscode
-          vscode-extensions.rust-lang.rust-analyzer-nightly
+          (vscode-with-extensions.override {
+            vscodeExtensions = with vscode-extensions; [
+              bbenoist.nix
+              ms-python.python
+              ms-azuretools.vscode-docker
+              ms-vscode-remote.remote-ssh
+            ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+              {
+                name = "remote-ssh-edit";
+                publisher = "ms-vscode-remote";
+                version = "0.47.2";
+                sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+              }
+            ];
+          })
+          # vscode
+          # vscode-extensions.rust-lang.rust-analyzer-nightly
         ];
 
         shellHook = ''
