4c939e7e25f726b0b2ea361607b76b30308cb42a
Craole <32288735+Craole@users.noreply.github.com>
2024-01-11 21:49:25 -0500

    adding rails

--- flake.nix
+++ flake.nix
@@ -5,36 +5,46 @@
 #|   │   ├── default.nix
 #|   │   └── devShell.nix
 #|   └── flake.nix
-
 #@ Project File
 #| flake.nix
 #>============================================<#
-
 {
   description = "Development Environment Templates";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     devenv.url = "github:cachix/devenv/latest";
-    templates_NixOS.url = github:NixOS/templates;
+    templates_NixOS.url = "github:NixOS/templates";
     templates_nix-way.url = "github:the-nix-way/dev-templates";
   };
 
-  outputs = { self, nixpkgs, devenv, templates_NixOS, templates_nix-way, ... }: {
-    templates = {
-      rustly = {
-        path = ./templates/rustly;
-        description = "Rust Development Environment [Nightly]";
-      };
-      rust-leptos = {
-        path = ./templates/rust_leptos;
-        description = "Rust Web Development Environment [Nightly, Leptos], using fenix";
-      };
-      rust-workspaces = {
-        path = ./templates/rust_workspaces;
-        description = "Rust Development Environment [Nightly] [Cargo Workspaces], using fenix";
-      };
-    }
-    // templates_NixOS.templates
-    // templates_nix-way.templates;
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    templates_NixOS,
+    templates_nix-way,
+    ...
+  }: {
+    templates =
+      {
+        rubyrails = {
+          path = ./templates/ruby_on_rails;
+          description = "Ruby on Rails";
+        };
+        rust-leptos = {
+          path = ./templates/rust_leptos;
+          description = "Rust Web Development Environment [Nightly, Leptos], using fenix";
+        };
+        rust-workspaces = {
+          path = ./templates/rust_workspaces;
+          description = "Rust Development Environment [Nightly] [Cargo Workspaces], using fenix";
+        };
+        rustly = {
+          path = ./templates/rustly;
+          description = "Rust Development Environment [Nightly]";
+        };
+      }
+      // templates_NixOS.templates
+      // templates_nix-way.templates;
   };
 }
