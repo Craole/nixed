9cadee6c08c34844af683a3e07186f64c9494db6
Craole <32288735+Craole@users.noreply.github.com>
2024-01-14 08:59:08 -0500

    Removed alias in ruby template flake because they don't work

--- export/ruby_on_rails/flake.nix
+++ export/ruby_on_rails/flake.nix
@@ -6,34 +6,27 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-    in {
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs; [
-          ruby_3_3
-          libyaml
-          postgresql
-        ];
+  outputs = { self, nixpkgs, flake-utils, }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ ruby_3_3 libyaml postgresql ];
 
-        shellHook = ''
-          # Ensure that Bundler installs gems in the current directory
-          export BUNDLE_PATH=./.bundle
-          export RUBY_BIN="./.bundle/ruby/3.3.0/bin/"
-          export PATH=$RUBY_BIN:$PATH
+          shellHook = ''
+            # | Ensure that Bundler installs gems in the current directory
+            export BUNDLE_PATH=./.bundle
+            export RUBY_BIN="./.bundle/ruby/3.3.0/bin/"
+            export PATH=$RUBY_BIN:$PATH
 
-          # Check for Gemfile and run bundle install if necessary
-          [ -f Gemfile ] && bundle install
+            # | Check for Gemfile and run bundle install if necessary
+            [ -f Gemfile ] && bundle install
 
-          ruby --version
-          postgres --version
-          rails --version
-        '';
-      };
-    });
+            # | Validate installation
+            ruby --version
+            postgres --version
+            rails --version
+          '';
+        };
+      });
 }
