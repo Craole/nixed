a977a364fa3454276b054caa899cdaedb546bb72
craole <32288735+Craole@users.noreply.github.com>
2024-08-30 08:01:08 -0500

    update the rust template - trust

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
 {
   description = "Development Environment Templates";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     # devenv.url = "github:cachix/devenv/latest";
     templates_NixOS.url = "github:NixOS/templates";
@@ -22,7 +22,7 @@
   outputs =
     {
       self,
-      nixpkgs,
+      # nixpkgs,
       # devenv,
       templates_NixOS,
       templates_nix-way,
@@ -51,5 +51,6 @@
           description = "Rust Development Environment";
         };
       } // templates_NixOS.templates // templates_nix-way.templates;
+      defaultTemplate = self.templates.trust;
     };
 }
